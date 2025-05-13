93442660bd635af913486d9cf7a9ce3aa8742bc2
Lerrica Torreno <155938425+Lerrica-Torreno@users.noreply.github.com>
2025-05-13 19:38:48 +0800

    contains the responses of the bot

---
+++ long_responses_bot.py
@@ -0,0 +1,18 @@
+# Name: Torreno, Lerrica Jeremy S.
+# Course/Year: BSCS-DS 1st YEAR
+# Date: November 9, 2023
+# Work title: Midterm Lab Work #1 
+
+import random
+
+# 10 trivial facts about coffee
+R_EFFECTS = "The coffee's stimulating effects were noticed by a goat herder named Kaldi in Ethiopia after noticing that his goats were alert after consuming berries from a coffee tree."
+R_CONS = "Drinking coffee first thing in the morning can actually hurt your body, as it tricks it into producing cortisol when it does not have to."
+R_BENEFITS = "Coffee is one of the healthiest beverages in the world because it is fully packed with antioxidants."
+R_PRODUCER = "Brazil is the world's biggest producer of coffee, they produce around 40% of the world's coffee."
+R_BEAN = "Coffee bean is not actually a bean, it comes from the seed that is found inside the coffee cherry."
+R_MOST_COFFEE = "In 2020, Netherlands was recorded as they consume the most coffee with 8.3 kilograms."
+R_EXPENSIVE = "Kopi Luwak is the most expensive coffee in the world that is from Indonesia and made from beans digested from the Asian Palm Civet or simply cat poop."
+R_COMMODITY = "Coffee is the second's largest traded commodity that is only surpassed by crude oil."
+R_CREATIVITY = "The soft music that you hear in coffee shops can actually boost your creativity, as it helps people to think outside the box by encouraging a flexible mindset."
+R_RCONTEXT = "Coffee was consumed under religious context, it was a favorite among religious circles, scholars, and intellectuals in the Muslims and Christian Europe."
